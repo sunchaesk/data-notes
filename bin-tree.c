7c75b7e4b1d4d93563e07e62237e0e0dda3d73f5
Chris Kim <sunchaesk@gmail.com>
2021-04-22 17:09:56 -0700

    priority q

--- bin-tree.c
+++ bin-tree.c
@@ -35,3 +35,44 @@ tree* search_tree(tree *l, int x){
     else
         return search_tree(l->right, x);
 }
+
+
+// Finding max and min
+// If max -> just go to the rightmost tree node
+// vice versa for min
+tree* find_minimum(tree *t){
+    tree* min;
+
+    if (t == NULL) return NULL;
+
+    min = t;
+    while (min->left != NULL)
+        min = min->left;
+    return min;
+}
+
+// Traversal - visiting all nodes in the binary tree
+void traverse_tree(tree *l){
+    while(l != NULL){
+        traverse_tree(l->left);
+        //process_item(l->item);
+        traverse_tree(l->right);
+    }
+}
+
+// Insert Node to bin tree
+void insert_tree(tree **l, int x, tree *parent){
+    tree *p;
+    if (*l == NULL){
+        p = malloc(sizeof(tree));
+        p->item = x;
+        p->left = p->right = NULL;
+        p->parent = parent;
+        *l = p;
+        return;
+    }
+    if (x < (*l)->item)
+        insert_tree(&((*l)->left), x, *l);
+    else
+        insert_tree(&((*l)->right), x, *l);
+}
---
+++ priority-q.c
@@ -0,0 +1,9 @@
+
+// Priority Queues - sorting them by importance
+/*
+ * Priority Queues operations
+ * - Insert(Q, x) - Given an item with key k, insert it
+ * - Find Min & Find Max
+ * - Delete Min & Delete Max
+ * NOTE: Build algorithms around data structures -> clean code
+ */
