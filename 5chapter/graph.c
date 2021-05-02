e319dfe249a021dfaa0f40664cb404b958ab2b1a
Chris Kim <sunchaesk@gmail.com>
2021-05-02 13:55:36 -0700

    chapter 4

---
+++ 5chapter/graph.c
@@ -0,0 +1,19 @@
+
+// Flavours of Graph
+// - Directed Graph:
+//    - If doesn't have (y, x) -> directed graph (has direction basically)
+//    - each vertex has an arrow pointing
+// - Undirected Graph:
+//    - If (x, y) in graph, (y, x) is also in the undirected graph
+//    - No arrow
+//
+// - Weight Graph:
+//   - Each edge (or vertex) is given a numerical value (weight)
+// - Unweighted Graph:
+//   - Each edge of vertex is not given a numerical value
+//
+// - Acyclic Graph:
+//     - does not contain any cycles
+// - Cyclic Graph:
+//    - has cycles in graphs
+//
