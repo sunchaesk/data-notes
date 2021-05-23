3708f1b23b7982f6b426c6b96e17219080ea1d9d
Chris Kim <sunchaesk@gmail.com>
2021-05-22 17:14:21 -0700

    recursion

---
+++ CLRS/1chapter/div-conquer.c
@@ -0,0 +1,15 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+// NOTE: Divide And Conquer
+// recursively solve the problem, and at each recursive stage, do 3 steps:
+// - Divide: problem -> subproblem : smaller instances of the same problem
+// - Conquer : recursively solve the subproblems. If the subprogram is small enough, solve the problem
+// - Combine:  Combine results from each part -> return 1
+
+int main(){
+
+}
